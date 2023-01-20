9bfed5b77c11877b17af769479d6929c1b24a81c
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-01-21 02:11:53 +0700

    Initial Commit

---
+++ Snowboard/SnowboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowboardApp.swift
+//  Snowboard
+//
+//  Created by Putut Yusri Bahtiar on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
